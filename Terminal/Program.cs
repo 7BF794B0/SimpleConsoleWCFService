b026e4a41b10b2de058f4a7a4c5f60264a6880f3
7BF794B0 <eremchenko.au@gmail.com>
2016-11-22 22:29:46 +0300

    Fix: 2,4,5

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -1,24 +1,42 @@
 ﻿using System;
+using System.Threading;
 
 namespace Terminal
 {
     class Program
     {
         /// <summary>
+        /// Создать новое подключение к серверу.
+        /// </summary>
+        /// <param name="obj">Id терминала</param>
+        private static void NewConnection(object obj)
+        {
+            using (var client = new ImpData.DataClient("NetTcpBinding_IData"))
+            {
+                client.SendData((int)obj);
+                client.Close();
+            }
+        }
+
+        /// <summary>
         /// The main entry point for the application.
         /// </summary>
         static void Main()
         {
-            Console.Write("Enter your id:");
-            int id = int.Parse(Console.ReadLine());
+            int interval;
+            Console.Write("Enter the value of re-treatment interval (in milliseconds): ");
+            while (!int.TryParse(Console.ReadLine(), out interval))
+                Console.Write("Error. Entered invalid value. Enter value of re-treatment interval (in milliseconds): ");
+
+            int id;
+            Console.Write("Enter your id: ");
+            while (!int.TryParse(Console.ReadLine(), out id))
+                Console.Write("Error. Entered invalid value. Enter your id: ");
 
             try
             {
-                using (var client = new ImpData.DataClient("NetTcpBinding_IData"))
-                {
-                    client.SendData(id);
-                    client.Close();
-                }
+                var tm = new TimerCallback(NewConnection);
+                var timer = new Timer(tm, id, 0, interval);
             }
             catch (Exception ex)
             {
